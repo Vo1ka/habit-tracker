095a4b5cadfc6ddb93c219fd52e0982ec6e6036e
Vo1ka <syhov2015@ya.ru>
2025-11-02 19:52:26 +0300

    feature: add localStorage save

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useEffect } from 'react';
 import { AddHabitForm } from './components/AddHabitForm/AddHabitForm';
 import { HabitList } from './components/HabitList/HabitList';
 import './index.css'
+import { loadFromLocalStorage } from './utils/localStorage';
+import { useAppDispatch } from './store/hooks';
+import { loadFromStorage } from './store/slices/habitsSlice';
 
 
 function App() {
+  const dispatch = useAppDispatch();
+  useEffect(()=>{
+    const response = loadFromLocalStorage();
+    if (response){
+      dispatch(loadFromStorage(response))
+    }
+  }, [])
 
   return (
     <div className="min-h-screen bg-gray-100 p-8">
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,15 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.ts'
+import { saveToLocalStorage } from './utils/localStorage.ts'
+
+store.subscribe(()=>{
+  const state = store.getState();
+  saveToLocalStorage({
+    habits: state.habits.habits,
+    logs: state.habits.logs
+  })
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
